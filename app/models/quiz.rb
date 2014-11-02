a955a4cf93c56eaa84907a622ba2f6fed253570c
Maciej Jaromin <maciej_jaromin@o2.pl>
2014-11-02 19:12:40 +0100

    Form formatting tweak and allowing destroying questions.

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -1,6 +1,6 @@
 class Quiz < ActiveRecord::Base
   belongs_to :category
   has_many :questions
-  accepts_nested_attributes_for :questions
+  accepts_nested_attributes_for :questions, allow_destroy: true
   validates :name, :description, :category, presence: true
 end
